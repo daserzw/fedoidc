6e88a4a1238c4f3c76802feb55dec22fa9e5ce58
Roland Hedberg <roland@catalogix.se>
2017-07-05 12:48:03 +0200

    Upped version number.

--- src/fedoidc/__init__.py
+++ src/fedoidc/__init__.py
@@ -20,7 +20,7 @@ from oic.oic.message import RegistrationRequest
 logger = logging.getLogger(__name__)
 
 __author__ = 'roland'
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 
 #: Contexts in which metadata statements can be used
 CONTEXTS = ['registration', 'discovery', 'response']
